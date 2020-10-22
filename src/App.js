2fa3c4d24237387ca074a823891080a16d9ea16b
clintjeez <clintjeez@gmail.com>
2020-10-22 22:25:11 +0100

    home banner setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./Header";
+import Home from "./Home";
 
 import "./App.css";
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
